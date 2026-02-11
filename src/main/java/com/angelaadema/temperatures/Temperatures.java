8bd64211ee49ed028af96a47d6aaedbb2a3c0627
Angi-Adema <dr.adema@gmail.com>
2026-02-11 16:40:26 -0700

    added notes to the codebase

--- src/main/java/com/angelaadema/temperatures/Temperatures.java
+++ src/main/java/com/angelaadema/temperatures/Temperatures.java
@@ -6,13 +6,16 @@ import java.util.Scanner;
 public class Temperatures {
 	public static void main(String[] args) {		
 		
+		// Create scanner object to read user input.
 		Scanner scnr = new Scanner(System.in);
 		
+		// Declare variables.
 		String weekDay;
 		int average;
 		int sum = 0;
 		int index;
 		
+		// Create ArrayList for days of the week.
 		ArrayList<String> day = new ArrayList<>();
 			day.add("Monday");
 			day.add("Tuesday");
@@ -21,7 +24,8 @@ public class Temperatures {
 			day.add("Friday");
 			day.add("Saturday");
 			day.add("Sunday");
-			
+		
+		// Create ArrayList for daily temperatures.
 		ArrayList<Integer> dailyTemp = new ArrayList<>();
 			dailyTemp.add(65);
 			dailyTemp.add(52);
@@ -31,23 +35,29 @@ public class Temperatures {
 			dailyTemp.add(55);
 			dailyTemp.add(63);
 			
-
+			// Prompt user to input a day or enter week for whole week.
 			System.out.println("Please enter a day of the week for temp on one day or enter 'week' for full week temps with average: ");
+			// Read user input.
 			weekDay = scnr.nextLine();	
 			
+			// Control flow statement to handle user input.
 			if (weekDay.equalsIgnoreCase("week")) {
 				
+				// Loop through the day ArrayList and output corresponding temperature as well as sum dailyTemps.
 				for (int i = 0; i < day.size(); i++) {
 					System.out.println(day.get(i) + ": " + dailyTemp.get(i));
-					sum += dailyTemp.get(i);
+					sum += dailyTemp.get(i);   
 				}
 				
+				// Calculate and output average week temperature.
 				average = sum / dailyTemp.size();
 				System.out.println("The week's average temp is: " + average);
 				
 			} else {
+				// Store index of input day to get corresponding temperature.
 				index = day.indexOf(weekDay);
 				
+				// Control flow statement to handle printing the entered day with the corresponding temperature or advise to try again.
 				if (index != -1) {
 					System.out.println("Your selected day is: " + weekDay + " and the temperature is expected to be: " + dailyTemp.get(index));
 				} else {
